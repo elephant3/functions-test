a015c95706770240d6a6ebcf5f41324231bc9e12
Siarhei Lipchyk <contact@siarhei.dev>
2022-03-14 13:47:21 +0100

    only one

--- functions/test2.js
+++ functions/test2.js
@@ -1,4 +1,4 @@
 export default (req, res) => {
   const { name = "World" } = req.query;
-  res.status(200).send(`Should deploy 2 apps ${name}!`);
+  res.status(200).send(`Should deploy only 1 app ${name}!`);
 };
