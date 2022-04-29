26cdc07bb1582a8ac60ccf00c84bbda51eee3679
Siarhei Lipchyk <contact@siarhei.dev>
2022-04-29 16:47:34 +0200

    Update test.js

--- functions/test.js
+++ functions/test.js
@@ -3,5 +3,8 @@ module.exports = (req, res) => {
   console.log(
     "This logs should be printed in dev and stored somewhere in prod"
   );
+  
+  await new Promise(resolve => setTimeout(resolve, 5000));
+  
   res.status(200).send(`PLEASE WORK  ${name}!`);
 };
